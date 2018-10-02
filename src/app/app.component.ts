b08dd4d97bfe841d50caa94bb59ac040bfd5d7ac
Chuck Hutchinson <chuck@perfectsensedigital.com>
2018-10-02 15:27:30 -0400

    added charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   title = 'AngularPrimeNGExamples';
 
   data: any;
+  linedata: any;
+  bardata: any;
+  
   constructor() {
     this.data = {
         labels: ['A','B','C'],
@@ -26,6 +29,42 @@ export class AppComponent {
                     "#FFCE56"
                 ]
             }]    
+        };
+
+      this.linedata = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderColor: '#565656'
+                }
+            ]
+        };
+
+      this.bardata = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#1E88E5',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: '#9CCC65',
+                    borderColor: '#7CB342',
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
         };
     }
 }
